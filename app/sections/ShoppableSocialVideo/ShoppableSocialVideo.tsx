24124d630dc75d7ae7895dbf6557dc5c1aad5f29
jeremyagabriel <jeremyagabriel@gmail.com>
2024-07-12 17:14:33 -0700

    chore: video playsinline

--- app/sections/ShoppableSocialVideo/ShoppableSocialVideo.tsx
+++ app/sections/ShoppableSocialVideo/ShoppableSocialVideo.tsx
@@ -83,6 +83,7 @@ export function ShoppableSocialVideo({cms}: {cms: ShoppableSocialVideoCms}) {
           loop
           muted
           controls={false}
+          playsInline
           poster={video?.poster?.src}
         />
 
